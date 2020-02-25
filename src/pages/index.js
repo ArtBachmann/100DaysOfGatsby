623ea27c5944fee36060f6a6bc400c6e9cc984b4
Art Bachmann <bachmann.art@gmail.com>
2020-02-25 21:32:54 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: 'purple', fontWeight: 'bold' }}>
+    <h1>Hello Me!</h1>
+    <p>Everything is Going to be All Right</p>
+    <img src="https://www.wikifeet.com/Bella_Hadid#&gid=1&pid=4790140" alt="" />
+  </div>
+) 
